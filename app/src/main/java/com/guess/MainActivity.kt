5a331e734dba2fd8715df7ba98f188cadd1ae6ec
David Lacour <davidlacourgaletto@gmail.com>
2023-03-02 20:32:44 +0100

    jetpack compose greeting app untested

--- app/src/main/java/com/guess/MainActivity.kt
+++ app/src/main/java/com/guess/MainActivity.kt
@@ -1,11 +1,54 @@
 package com.guess
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.text.TextLayoutResult
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.input.VisualTransformation
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            simpleButton()
+
+        }
+
+
+    }
+
+    @Composable
+    fun simpleButton() {
+        var text by remember { mutableStateOf(TextFieldValue("")) }
+        var intent = Intent(this, GreetingActivity::class.java)
+        Column() {
+            Button(onClick = {
+                intent.putExtra("message_key", text.text.toString())
+                startActivity(intent)
+            }) {
+                Text(text = "simple Button")
+
+            }
+            TextField(
+                value = text,
+                onValueChange = { newText ->
+                    text = newText
+                }
+            )
+        }
     }
 }
